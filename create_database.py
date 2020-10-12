4bbfeca176744f9ffb797d4c9dddc3c8147ca408
Hari Haran S <hariharankvasn@gmail.com>
2020-10-12 10:05:12 +0530

    Add files via upload

---
+++ create_database.py
@@ -0,0 +1,51 @@
+# create_database.py
+import cv2, sys, numpy, os, time
+count = 0
+size = 4
+fn_haar = 'haarcascade_frontalface_default.xml'
+dirr = 'database'
+fn_name = input("Enter Name of the person : ")   #name of the person
+
+(im_width, im_height) = (92, 72)
+haar_cascade = cv2.CascadeClassifier(fn_haar)
+webcam = cv2.VideoCapture(0)
+print ("-----------------------Taking pictures----------------------")
+print ("--------------------Give some expressions---------------------")
+# The program loops until it has 20 images of the face.
+
+model = cv2.face.LBPHFaceRecognizer_create()
+try:
+    model.read('model.xml')
+    i = model.getLabels()[-1][0] + 1
+except:
+    i = 0
+(images, lables, names, id) = ([], [], {}, 0)
+
+while count < 10:
+    (rval, im) = webcam.read()
+    im = cv2.flip(im, 1, 0)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    mini = cv2.resize(gray, (gray.shape[1] // size, gray.shape[0] // size))
+    faces = haar_cascade.detectMultiScale(mini)
+    faces = sorted(faces, key=lambda x: x[3])
+    if faces:
+        face_i = faces[0]
+        (x, y, w, h) = [v * size for v in face_i]
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (im_width, im_height))
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.putText(im, fn_name, (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+        images.append(face_resize)
+        lables.append(int(i))
+        count += 1
+
+    cv2.imshow('OpenCV', im)
+    key = cv2.waitKey(10)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+
+#Create a Numpy array from the two lists above
+(images, lables) = [numpy.array(lis) for lis in [images, lables]]
+
+model.update(images, lables)
+model.save('model.xml')
---
+++ face_rec.py
@@ -0,0 +1,46 @@
+# facerec.py
+import cv2, sys, numpy, os, time
+size = 4
+fn_haar = 'haarcascade_frontalface_default.xml'
+fn_dir = 'database'
+haar_cascade = cv2.CascadeClassifier(fn_haar)
+def pathdir(fn_nam):
+    path = os.path.join(fn_dir, fn_nam)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+(im_width, im_height) = (200, 175)
+
+#OpenCV trains a model from the images
+model = cv2.face.LBPHFaceRecognizer_create()
+model.read('model.xml')
+
+names = ['Hari', 'Sundar', 'Vaishu', 'Pam', 'Sundhachu',]
+# Part 2: Use LBPHRecognizer on camera stream
+face_cascade = cv2.CascadeClassifier(fn_haar)
+webcam = cv2.VideoCapture(0)
+count=0
+while True:
+    (rval, im) = webcam.read()
+    im = cv2.flip(im, 1, 0)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.7,
+        minNeighbors=2,
+        minSize=(50, 50)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+    star=time.time()
+    for (x,y,w,h) in faces:
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (im_width, im_height))
+        # Try to recognize the face
+        prediction = model.predict(face_resize)
+        cv2.rectangle(im, (x, y), (x + w+10, y + h+10), (0, 255, 0), 2)
+
+        if prediction[1]<=120 and prediction[1]>=40:
+            print(names[prediction[0]])
+    cv2.imshow('..Facial Recogonision..', im)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
