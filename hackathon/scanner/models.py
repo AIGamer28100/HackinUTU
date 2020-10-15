9a64e71f40313c17c1a2fd6276d4e7e43f6acb73
Hari Haran S <hariharankvasn@gmail.com>
2020-10-15 12:21:00 +0530

    Update models.py

--- hackathon/scanner/models.py
+++ hackathon/scanner/models.py
@@ -3,17 +3,34 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 # Model 1
-
-
 class Category(models.Model):
-    category = models.CharField(max_length=20, blank=False, null=False)
+    category = models.CharField(max_length = 20, blank = False, null = False, )
 
     def __str__(self):
         return f"{self.category}"
 
+    
 # Model 2
+class RTO(models.Model):
+    code = models.PositiveIntegerField(max_length = 2, min_length = 2, )
+    def __str__(self):
+        return f"{self.code}";
 
+    
+# Model 3
+class State(models.Model):
+    state = models.CharField(max_length = 2, blank = False, null = False, )
+    RTO_code = models.ManyToManyField(RTO, on_delete = models.CASCADE, blank = False, default = None, )
+    def __str__(self):
+        return f"{self.state}";
 
+    
+# Model 4
 class License(models.Model):
-    name = models.CharField(max_length=70)
-    dateOfIssue = models.DateTimeField(auto_now=False, auto_now_add=False)
+    DL_No = models.PositiveIntegerField(max_length = 7, min_length = 7, )
+    name = models.CharField(max_length = 70, )
+    dateOfIssue = models.DateTimeField(auto_now = False, auto_now_add = False, )
+    validTill = models.DateTimeField(auto_now = False, auto_now_add = False, )
+    state = models.ForeignKey(State, on_delete = models.CASCADE, blank = False, default = None, )
+    category = models.ManyToMany(Category, blank = True, )
+    picture = models.ImageField(upload_to='ProfilesPics')
