caccd3c0e1c9af0193c52908b8ef9e32fa2a9ced
Coding-Maniac <ufoundsanjay@gmail.com>
2020-10-15 11:52:27 +0530

    Basic Model [HariHHaran]

--- hackathon/scanner/models.py
+++ hackathon/scanner/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+# Model 1
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.category}"
+
+# Model 2
+
+
+class License(models.Model):
+    name = models.CharField(max_length=70)
+    dateOfIssue = models.DateTimeField(auto_now=False, auto_now_add=False)
