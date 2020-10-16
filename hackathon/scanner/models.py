a58b72a34d8d2f2012519f5b3309e8ff78add713
AIGAMER <hariharankvasn@gmail.com>
2020-10-16 09:26:16 +0530

    Model changes

--- hackathon/scanner/models.py
+++ hackathon/scanner/models.py
@@ -9,28 +9,28 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.category}"
 
-    
+
 # Model 2
 class RTO(models.Model):
-    code = models.PositiveIntegerField(max_length = 2, min_length = 2, )
+    code = models.PositiveIntegerField(max_length = 2, )
     def __str__(self):
         return f"{self.code}";
 
-    
+
 # Model 3
 class State(models.Model):
     state = models.CharField(max_length = 2, blank = False, null = False, )
-    RTO_code = models.ManyToManyField(RTO, on_delete = models.CASCADE, blank = False, default = None, )
+    RTO_code = models.ManyToManyField(RTO, blank = False, default = None, )
     def __str__(self):
         return f"{self.state}";
 
-    
+
 # Model 4
 class License(models.Model):
-    DL_No = models.PositiveIntegerField(max_length = 7, min_length = 7, )
+    DL_No = models.PositiveIntegerField(max_length = 7, )
     name = models.CharField(max_length = 70, )
     dateOfIssue = models.DateTimeField(auto_now = False, auto_now_add = False, )
     validTill = models.DateTimeField(auto_now = False, auto_now_add = False, )
     state = models.ForeignKey(State, on_delete = models.CASCADE, blank = False, default = None, )
-    category = models.ManyToMany(Category, blank = True, )
+    category = models.ManyToManyField(Category, blank = True, )
     picture = models.ImageField(upload_to='ProfilesPics')
